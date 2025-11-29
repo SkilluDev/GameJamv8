3f46a14a0bf82a51d82d17bd5dc7d8674bbc95cb
s32087 <s32087@pjwstk.edu.pl>
2025-11-29 18:31:00 +0100

    Fix bugs

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,6 +7,8 @@ public class PlayerInput : MonoBehaviour
     public UnityEvent onMouseClick;
     [SerializeField] private Transform cameraPosition;
 
+    [SerializeField] private LayerMask enemyLayer;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -22,8 +24,12 @@ public class PlayerInput : MonoBehaviour
 
         if(Physics.Raycast(cameraPosition.position, cameraPosition.TransformDirection(Vector3.forward),  out hit, 100f))
         {
-            Debug.DrawRay(cameraPosition.position, cameraPosition.TransformDirection(Vector3.forward) * 100f, Color.red);
-            Debug.Log("hittt"+hit.distance);
+            Debug.DrawRay(cameraPosition.position, cameraPosition.TransformDirection(Vector3.forward) * hit.distance, Color.red, 20f);
+            Debug.Log("hittt" + hit.distance+"layer"+hit.transform.gameObject.layer);
+            if((enemyLayer & (1 << hit.transform.gameObject.layer)) != 0)
+            {
+                Debug.Log("enemy");
+            }
         }
     }
 }
