ed9cdd0bf3db942f473132a38ba75f1c67398060
s32087 <s32087@pjwstk.edu.pl>
2025-11-29 22:54:05 +0100

    Make mushrooms spawn

--- Assets/Scripts/SpawnEntities.cs
+++ Assets/Scripts/SpawnEntities.cs
@@ -2,14 +2,17 @@ using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SpawnEnemies : MonoBehaviour
+public class SpawnEntities : MonoBehaviour
 {
     [SerializeField] float spawnRange = 50.0f;
+    [SerializeField] int enemyCount = 30;
+    [SerializeField] int mushroomCount = 10;
     [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject mushroomPrefab;
 
     bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < 1000; i++)
         {
             Vector3 randomPoint = center + Random.insideUnitSphere * range;
             NavMeshHit hit;
@@ -24,13 +27,23 @@ public class SpawnEnemies : MonoBehaviour
     }
     void Start()
     {
-        for(int i = 0; i < 30; i++)
+        for(int i = 0; i < enemyCount; i++)
         {
             Vector3 point;
             if (RandomPoint(transform.position, spawnRange, out point))
             {
                 Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
                 Instantiate(enemyPrefab, point+Vector3.up*1.68f, Quaternion.identity);
+            }
+        }
+
+        for(int i = 0; i < mushroomCount; i++)
+        {
+            Vector3 point;
+            if (RandomPoint(transform.position, spawnRange, out point))
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                Instantiate(mushroomPrefab, point+Vector3.up*1.68f, Quaternion.identity);
             }
         }
         
